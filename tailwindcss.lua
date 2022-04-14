b2ae0d2852f258d3bfa74be4cd9b142c6efeb17e
gabriel-antonelli <gabrielantonelli25@gmail.com>
2022-04-14 19:24:39 -0300

    remove unecessary files and add tailwind.lua to lvim folder

--- tailwindcss.lua
+++
@@ -1,7 +0,0 @@
-local project_has_tailwindcss_dependency = function()
-  return (vim.fn.glob "tailwind*" ~= "")
-end
-
-if project_has_tailwindcss_dependency() == true then
-  require("lvim.lsp.manager").setup "tailwindcss"
-end
